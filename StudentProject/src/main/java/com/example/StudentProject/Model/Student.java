11ed6dcd1cd754549cc31452ffc52d0bfe205104
GUNAVARDHAN JAKKIDI <vardhanreddy226@gmail.com>
2023-02-05 17:30:33 -0500

    Adding the student project

---
+++ StudentProject/src/main/java/com/example/StudentProject/Model/Student.java
@@ -0,0 +1,40 @@
+package com.example.StudentProject.Model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	public Student() {
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	private String name;
+	private String Address;
+	
+
+}
